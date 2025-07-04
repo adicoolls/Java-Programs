4b128cb71b8e37ea1cf1293f4c81f7df4b160c62
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-04 10:11:04 +0530

    5th program

---
+++ Desktop/java progrms/college5.java
@@ -0,0 +1,20 @@
+// public class college5 {
+// public static void main(String[] args) {
+//     // syntax of while loop
+//     //initialization
+//     //while(conditiion){
+//         //code to be executed
+//         //increment or decrement
+//    //}
+// }
+// }
+
+public class college5 {
+    public static void main(String[] args) {
+        int i = 10;
+        while(i <=20){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
