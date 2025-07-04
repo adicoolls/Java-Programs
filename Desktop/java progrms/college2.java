32b4de635146298d6db97ed1502c1b2146ebb8b9
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-04 10:08:32 +0530

    2nd program

---
+++ Desktop/java progrms/college2.java
@@ -0,0 +1,17 @@
+public class college2 {
+    public static void main(String[] args) {
+        int a, b, c, d, e;
+        a = 45; 
+        b = 56;
+        c = 34;
+        d = 67;
+        e = 90;
+        
+       int totalmarks = a+b+c+d+e;
+       int avg = totalmarks/5;
+
+       System.out.println("the average is " + avg);
+
+
+    }
+}
