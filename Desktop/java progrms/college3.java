aca8ec9d087345130947718925744357368d5c6c
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-04 10:09:43 +0530

    3rd program

---
+++ Desktop/java progrms/college3.java
@@ -0,0 +1,14 @@
+public class college3 {
+    public static void main(String[] args) {
+        //syntax of for loop
+        //for(initialization;condition;increment/decrement){
+        //code to be executed; 
+        //}
+        
+        for(int i = 0; i<=10; i++){
+            System.out.print(i);
+
+        }
+        }
+    }
+
