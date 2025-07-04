c371708ac482c852289ece8de9c64a1c98508123
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-04 10:13:18 +0530

    8th program

---
+++ Desktop/java progrms/college8.java
@@ -0,0 +1,19 @@
+public class college8 {
+    public static void main(String[] args) {
+        //syntax of switch
+        /*
+         * switch(expression){
+         *  case 1:
+         *       code to be executed;
+         *       break;
+         *   case 2:
+         *       code to be executed;
+         *       break;
+         *   default:
+         *       code to be executed;
+         }
+         */
+        
+         
+    }
+}
