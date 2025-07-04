bd26f29353813391fb78a47458a831dffa6e6f98
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-04 10:10:28 +0530

    4th program

---
+++ Desktop/java progrms/college4.java
@@ -0,0 +1,9 @@
+public class college4{
+    public static void main(String[] args) {
+        int n = 56;
+        for(int i = 0; i<=n; i++)
+        if(i % 2 == 0 || i % 5 == 0)
+        System.out.println(i);
+    }
+}
+
