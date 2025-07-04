a59b9a55977ae88f47db9f0470c9ffe1bd435fe7
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-04 10:12:36 +0530

    7th program

---
+++ Desktop/java progrms/college7.java
@@ -0,0 +1,22 @@
+ public class college7 {
+//     public static void main(String[] args) {
+//         //syntax of do while loop
+//        // int i = 1;
+//        // do{
+           
+//        //}
+//        //while(condition);
+//        // next statement;
+//     }
+// }
+
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i <= 10);
+    }
+
+ }
