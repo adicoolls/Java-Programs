c024606232beef48d192cf203b624fef7297bb21
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-04 10:06:43 +0530

    1st program

---
+++ Desktop/java progrms/college1.java
@@ -0,0 +1,13 @@
+public class college1 {
+ 
+    public static void main(String[] args) {
+        int a = 10;
+int b = 20;
+int temp;
+
+temp = a;
+a = b;
+b = temp;
+System.out.println("a = "+ a + "b = " + b);
+    }
+}
