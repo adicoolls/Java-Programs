f948eed7d52efdbaa3a4a951131c0b462b3f66c0
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-04 10:15:24 +0530

    10th program

---
+++ Desktop/java progrms/college10.java
@@ -0,0 +1,19 @@
+public class college10 {
+ 
+    static void area(double l , double b){
+        
+        double area = l*b;
+        System.out.println("the area of the rectangle is "+area);
+    }
+    public static void main(String[] args) {
+        
+        double l = 10, b = 20;
+        area(l, b);
+    }
+}
+
+/*
+ * public class college{
+ * stati
+ * }
+ */
