8fba45b77dbc9125596744b5bb706565a83c3e2c
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-09 22:06:48 +0530

    the swapping program of java

---
+++ Desktop/java progrms/swap_using_3rdnumber.java
@@ -0,0 +1,9 @@
+public class swap_using_3rdnumber {
+    public static void main(String[] args) {
+        int a = 5, b = 4, c ;
+        c = a;
+        a = b;
+        b = c;
+        System.out.println("value are "+a+"and"+b);
+    }
+}
