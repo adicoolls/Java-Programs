4f51bbbc74f07919752ddb632c4aab3085da3d44
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-09 22:04:16 +0530

    java program

---
+++ Desktop/java progrms/oddeven.java
@@ -0,0 +1,10 @@
+public class oddeven {
+    public static void main(String[] args) {
+        int n = 7;
+        if(n%2==0){
+            System.out.println("the number is even");
+        }else{
+            System.out.println("the number is odd");
+        }
+    }
+}
