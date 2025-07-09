37944ebf9d393fb02599e79ec6558f45145c0aac
adicoolls <160615870+adicoolls@users.noreply.github.com>
2025-07-09 22:08:15 +0530

    swap program with out 3rd variable

---
+++ Desktop/java progrms/swap_without_3rdnumber.java
@@ -0,0 +1,11 @@
+public class swap_without_3rdnumber {
+    public static void main(String[] args) {
+       int a = 5, b = 4, c;
+        a = a+b;
+        b = a-b;
+        a= a-b;
+        System.out.println("the value is "+a+"and"+b);
+        
+
+    }
+}
